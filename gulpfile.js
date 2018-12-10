7ccb98034529f421e9bc4290bdd7ae7684715891
sun75 <1179431585@qq.com>
2018-12-10 09:47:12 +0800

    header over

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+gulp.task('default', function() {
+    // 将你的默认的任务代码放在这
+    console.log('shabi');
+  });
+gulp.task('first',function(){
+  console.log('first');
+});
+gulp.task('second', function(){
+  console.log('second');
+});
+gulp.task('third',function(){
+  console.log('thired');
+})
