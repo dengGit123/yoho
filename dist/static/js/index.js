df24d0520b0b8f721efc4ef83b630aaf41480fc0
BOxueXi <1664076161@qq.com>
2018-12-13 09:40:29 +0800

    图片修改

--- dist/static/js/index.js
+++ dist/static/js/index.js
@@ -1,6 +1,11 @@
 var colseCode = (function(){
 	var $dcCha = document.querySelector('.dc-cha');
+<<<<<<< HEAD
 	var $codeDownBox = document.querySelector('.code-down-box');
+=======
+	console.log($dcCha);
+	var $codeDownBox = document.querySelector('#code-down-box')
+>>>>>>> c39d9cdd1e916541c75d8c6a2b0e5addc8ecf703
 	return{
 		init(){
 			this.event();
@@ -8,7 +13,7 @@ var colseCode = (function(){
 		event(e){
 			var e = e || window.event;
 			$dcCha.onclick = function(){
-			$codeDownBox.style.display = 'none';
+			$codeDownBox.style.display = "none";
 			}
 		}
 	}
