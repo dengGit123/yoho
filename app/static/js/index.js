a284e8d929ea9f78faeb3b0cdccc80bc95070c56
sun75 <1179431585@qq.com>
2018-12-13 17:20:59 +0800

    修复了iconfont得bug

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,11 +1,8 @@
 var colseCode = (function(){
 	var $dcCha = document.querySelector('.dc-cha');
-<<<<<<< HEAD
 	//console.log($dcCha);
-=======
 	var $codeDownBox = document.querySelector('.code-down-box');
-	console.log($dcCha);
->>>>>>> 34537cfaece534aab2bb2e2a27aded9e91d6ba71
+	//console.log($dcCha);
 	var $codeDownBox = document.querySelector('#code-down-box')
 	return{
 		init(){
