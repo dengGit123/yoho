314f22fa8c4a702a1a9fd4c8e49697bf3ed95d7d
BOxueXi <1664076161@qq.com>
2018-12-13 09:49:03 +0800

    wuxiugai

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,11 +1,8 @@
 var colseCode = (function(){
 	var $dcCha = document.querySelector('.dc-cha');
-<<<<<<< HEAD
 	var $codeDownBox = document.querySelector('.code-down-box');
-=======
 	console.log($dcCha);
 	var $codeDownBox = document.querySelector('#code-down-box')
->>>>>>> c39d9cdd1e916541c75d8c6a2b0e5addc8ecf703
 	return{
 		init(){
 			this.event();
