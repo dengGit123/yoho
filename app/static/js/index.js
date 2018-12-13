be196586fba6891d096b9fab0440cc8ad4f045ee
sun75 <1179431585@qq.com>
2018-12-13 21:04:00 +0800

    首页合并

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -19,3 +19,33 @@ var colseCode = (function(){
 	}
 }())
 colseCode.init();
+
+
+//banner
+$(function () {
+    var which = 0;
+    $('.banner-tip li').click(function () {
+        which = $(this).index();
+        showImage(which);
+    });
+    function showImage() {
+        $('.banner-tip li').removeClass('active');
+        $('#banner .banner-inner img').stop().animate({'opacity':0,'z-index':1});
+        $('.banner-tip li').eq(which).addClass('active');
+        $('#banner .banner-inner img').eq(which).stop().animate({'opacity':1,'z-index':2});       
+    }
+    function autoPlay() {
+        which += 1;
+        if (which>7){
+            which = 0;
+        }else if (which<0){
+            which = 7;
+        }
+        showImage(which);
+    }
+    $('#banner').mouseleave(autoPlay());
+    var start = setInterval(function () {
+        autoPlay();
+    },2000)
+
+}())
